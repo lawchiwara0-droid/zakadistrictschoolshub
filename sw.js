ac176fa354679330bc29192b65e66ab6ee18d91f
lawsonchiwara <lawchiwara0@gmail.com>
2026-09-08 19:51:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,6 @@
-const CACHE = 'zaka-hub-v5';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+  "./",
+  "./index.html",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE)
-      .then(cache => cache.addAll(ASSETS))
-      .catch(() => {}) // don't fail install if the CDN is unreachable at install time
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      return cached || fetch(e.request).then(res => {
-        const copy = res.clone();
-        caches.open(CACHE).then(cache => cache.put(e.request, copy));
-        return res;
-      }).catch(() => cached);
-    })
-  );
-});
