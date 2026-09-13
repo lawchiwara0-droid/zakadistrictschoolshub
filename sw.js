3b2b9dfac5006b7739c2e29afa4cbba50b621740
lawsonchiwara <lawchiwara0@gmail.com>
2026-09-13 07:49:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,39 +1,50 @@
-const CACHE = 'zaka-hub-v5-6';
-const ASSETS = [
+const CACHE_VERSION = 'zaka-hub-v5-1-exam-test';
+const urlsToCache = [
   './',
   './index.html',
   './manifest.webmanifest',
   './icon-192.png',
-  './icon-512.png',
-  './zimsec_alevel_timetable_nov2026.pdf',
-  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+  './dummy_english_4005_01.pdf',
+  './dummy_literature_4029_01.pdf',
+  './dummy_frs_4047_01.pdf',
+  './zimsec_olevel_timetable_nov2026.pdf'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE)
-      .then(cache => cache.addAll(ASSETS))
-      .catch(() => {}) // don't fail install if the CDN is unreachable at install time
-      .then(() => self.skipWaiting())
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_VERSION).then(cache => {
+      return cache.addAll(urlsToCache).catch(err => {
+        console.log('Some resources failed to cache (this is OK for optional files like PDFs):', err);
+        return cache.addAll(urlsToCache.filter(url => !url.includes('.pdf')));
+      });
+    })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_VERSION) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      return cached || fetch(e.request).then(res => {
-        const copy = res.clone();
-        caches.open(CACHE).then(cache => cache.put(e.request, copy));
-        return res;
-      }).catch(() => cached);
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => {
+        return caches.match('./index.html');
+      });
     })
   );
 });
